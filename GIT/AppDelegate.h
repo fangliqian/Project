69bbb59a42332be3d1b8b6ce6c84104b7e9b0c93
fangliqian <fangliqian1@163.com>
2016-06-17 12:25:21 +0800

    保存的文件

---
+++ GIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT
+//
+//  Created by ma c on 16/6/17.
+//  Copyright © 2016年 ma c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIT
+//
+//  Created by ma c on 16/6/17.
+//  Copyright © 2016年 ma c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
